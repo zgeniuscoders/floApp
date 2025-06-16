6b104a2669012664ffedf79f563b6ac18ac27c8a
xman <izakikunda@gmail.com>
2025-06-16 13:16:35 +0100

    chore: interface pour representer la response

--- app/src/main/java/cd/zgeniuscoders/floapp/utilis/Response.kt
+++ app/src/main/java/cd/zgeniuscoders/floapp/utilis/Response.kt
@@ -1,2 +1,6 @@
 package cd.zgeniuscoders.floapp.utilis
 
+sealed class Response<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?) : Response<T>(data)
+    class Error<T>(message: String, data: T? = null) : Response<T>(data, message)
+}
