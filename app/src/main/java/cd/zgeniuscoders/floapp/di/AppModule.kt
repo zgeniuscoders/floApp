ee78b8d52b9f1c0b8d9724a611324668ca566b7e
xman <izakikunda@gmail.com>
2025-06-16 14:37:24 +0100

    feat: creation du fichier appModule pour gerer les dependances

--- app/src/main/java/cd/zgeniuscoders/floapp/di/AppModule.kt
+++ app/src/main/java/cd/zgeniuscoders/floapp/di/AppModule.kt
@@ -1,2 +1,36 @@
 package cd.zgeniuscoders.floapp.di
 
+import android.app.Application
+import android.content.Context
+import cd.zgeniuscoders.floapp.remote.AuthenticationService
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApplicationContext(application: Application): Context? {
+        return application.applicationContext
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestoreService(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCategoryRepository(db: FirebaseFirestore): AuthenticationService {
+        return AuthenticationService()
+    }
+
+
+}
