5d45f8df9f33d51cdb3319a45c9d8d76d58661ba
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-01 07:40:45 +0100

    feat: création modele History.kt

---
+++ app/src/main/java/cd/zgeniuscoders/floapp/models/History.kt
@@ -0,0 +1,9 @@
+package cd.zgeniuscoders.floapp.models
+
+data class History(
+    val id: String = "",
+    val name: String = "",
+    val paymentMethod: String = "",
+    val amount: String = "",
+    val paidAt: String ="",
+)
